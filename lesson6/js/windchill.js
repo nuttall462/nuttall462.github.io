de064e04b29cc2e9d6f45dc15526238af49beacf
nuttall462 <nut17001@byui.edu>
2020-10-24 00:09:22 -0600

    yas

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,5 +1,5 @@
-let temperature = parseFloat(document.getElementsById("temp").innerText);
-let windSpeed = parseFloat(document.getElementsById("speed").innerText);
+let temperature = parseFloat(document.getElementById("temp").innerText);
+let windSpeed = parseFloat(document.getElementById("speed").innerText);
 let windChill = parseInt(35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16)));
 
 if (temperature <= 50 && windSpeed >= 3.0) {
