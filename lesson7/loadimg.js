cb79752c7ec44cf87f7cbe48b75a1d7f989d504c
nuttall462 <nut17001@byui.edu>
2020-10-29 18:43:11 -0600

    get it

--- lesson7/loadimg.js
+++ lesson7/loadimg.js
@@ -2,34 +2,29 @@ let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px -100px 0px"
+    rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+  image.onload = () => {image.removeAttribute('data-src');};
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-
   if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
+    const imgObserver = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
-        if(item.isIntersecting) {
+        if (item.isIntersecting){
           loadImages(item.target);
-          observer.unobserve(item.target);
+          imgObserver.unobserve(item.target);
         }
       });
     });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
+    imagesToLoad.forEach((img) =>{
+      imgObserver.observe(img);
     });
+   
   } else {
-    imagesToLoad.forEach((img) => {
+    imagesToLoad.forEach((img) =>{
       loadImages(img);
     });
   }
