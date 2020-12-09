50761ae23b80dc7bf27410a62675d2b6e7bb7a19
nuttall462 <nut17001@byui.edu>
2020-12-08 21:38:52 -0700

    cmon

---
+++ final-project/js/rentals.js
@@ -0,0 +1,46 @@
+var rentalinfo = require("./rentals.json");
+var request = new XMLHttpRequest();
+var i;
+
+var rental = document.querySelector('.rental');
+
+request.open('GET', rentalinfo);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    var rentals = request.response;
+    scootsRentals(rentals);
+}
+
+function scootsRentals(jsonObj) {
+    var rentals = jsonObj['rentals'];
+    for(i = 0; i < rentals[i].rentals.length; i++){
+        var rentSection = document.createElement('section');
+
+        var rname = document.createElement('h2');
+        rname.textContent = rentals[i].name;
+        rentSection.appendChild(rname);
+
+        var rmax = document.createElement('p');
+        rmax.textContent = rentals[i].max;
+        rentSection.appendChild(rmax);
+
+        var reserve = document.createElement('p');
+        reserve.textContent = rentals[i].reservations[0];
+        rentSection.appendChild(reserve);
+
+        var reserve = document.createElement('p');
+        reserve.textContent = rentals[i].reservations[1];
+        rentSection.appendChild(reserve);
+
+        var walk = document.createElement('p');
+        walk.textContent = rentals[i].walkin[0];
+        rentSection.appendChild(walk);
+
+        var walk = document.createElement('p');
+        walk.textContent = rentals[i].walkin[1];
+        rentSection.appendChild(walk);
+    }
+    rental.appendChild(rentSection);
+}
+
---
+++ final-project/js/weather.js
@@ -0,0 +1,2 @@
+const weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=20.4230&lon=-86.9223&units=imperial&appid=b7011acaa1508a99e056c0882571c332";
+
