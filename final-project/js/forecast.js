851dc87beb79df0da8792b9416496a2083651675
nuttall462 <nut17001@byui.edu>
2020-12-10 00:20:07 -0700

    get  it

---
+++ final-project/js/forecast.js
@@ -0,0 +1,26 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=20.4230&lon=-86.9223&units=imperial&appid=b7011acaa1508a99e056c0882571c332";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    const forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+    console.log(forecast);
+    let day = 0;
+    const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+    
+    
+    forecast.forEach(x =>{
+        const d = new Date(forecast[day].dt_txt);
+        const isrc = 'https://openweathermap.org/img/w/' + forecast[day].weather[0].icon + '.png';
+        const desc = forecast[day].weather[0].description;
+        
+        document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+        document.getElementById(`forecast${day+1}`).textContent = x.main.temp + '\xB0F';
+        document.getElementById(`icon${day+1}`).setAttribute('src', isrc);
+        document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+        
+        day++;
+    })
+  });
--- final-project/js/weather.js
+++ final-project/js/weather.js
@@ -1,2 +1,16 @@
-const weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=20.4230&lon=-86.9223&units=imperial&appid=b7011acaa1508a99e056c0882571c332";
+const weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=20.4230&lon=-86.9223&units=imperial&exclude=hourly,daily&appid=b7011acaa1508a99e056c0882571c332";
 
+fetch(weatherURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    const humidity = document.querySelector('#humidity');
+    const current = document.querySelector('#currently');
+    const temp = document.querySelector('#current-temp');
+    
+    temp.innerHTML = jsObject.current.temp;
+    current.innerHTML = jsObject.current.weather[0].description;
+    humidity.innerHTML = jsObject.current.humidity;
+
+  });
