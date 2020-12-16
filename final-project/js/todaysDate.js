c35c8071766eb952af4a72e4c8e1ec0e03aa976b
nuttall462 <nut17001@byui.edu>
2020-12-16 13:06:56 -0700

    get it

--- final-project/js/todaysDate.js
+++ final-project/js/todaysDate.js
@@ -1,16 +1,1 @@
-try {
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
-  } catch (e) {
-    alert('Error with code or your browser does not support Locale');
-  }
-
-var d = new Date();
-var x = d.getDay();
-
-if (x==5){
-  document.getElementById("pancakes").style.display = "block";
-}
-else {
-  document.getElementById("pancakes").style.display = "none";
-}
+document.getElementById("currentDate").innerHTML = document.lastModified;
